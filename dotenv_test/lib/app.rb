22d284563964898f72766f138dadf68b83d1dbad
talandrius <talandrius@yahoo.fr>
2020-10-07 10:43:30 +0200

    twitter send

--- dotenv_test/lib/app.rb
+++ dotenv_test/lib/app.rb
@@ -1,10 +1,20 @@
 require 'dotenv'# Appelle la gem Dotenv
+# ligne très importante qui appelle la gem.
+require 'twitter'
 
-Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
-# et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+Dotenv.load
+# n'oublie pas les lignes pour Dotenv ici…
+
+# quelques lignes qui appellent les clés d'API de ton fichier .env
+client = Twitter::REST::Client.new do |config|
 
 # Il est ensuite très facile d'appeler les données du hash ENV, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
-puts ENV['TWITTER_API_SECRET']
 
-#Autre exemple 
-puts ENV['BEST_WEBSITE_EVER']
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+
+# ligne qui permet de tweeter sur ton compte
+client.update('Google Workspace arrive #google #workspace https://cutt.ly/FgezRk2 !!!!')
